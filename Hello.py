36baf683bdd63db2ffc7b2679bd40ea135d929cf
MLSE <semassel_medlamine@yahoo.fr>
2021-08-22 23:28:42 +0200

    First commit
    First commit

---
+++ Hello.py
@@ -0,0 +1,11 @@
+
+
+def show_message(name):
+    print ('%s %s %s' % ('*' * 20, name, '*' * 20))
+
+def main():    
+    show_message('start privacy now')
+
+
+if __name__ == '__main__':
+     main()                  
